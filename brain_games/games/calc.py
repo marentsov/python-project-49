d4e0c5700f24114734af7a3ae13c90398edd1e51
marentsov <mr.marentsov@gmail.com>
2024-12-18 16:20:37 +0300

    update game calc

---
+++ brain_games/games/calc.py
@@ -0,0 +1,26 @@
+from random import randint, choice
+RULES = 'What is the result of the expression?'
+
+def generate_game():
+
+    random_num_1 = randint(0, 20)
+    random_num_2 = randint(0, 20)
+    random_symbol = choice('+-*')
+
+    if random_symbol == '+':
+        result = random_num_1 + random_num_2
+    elif random_symbol == '-':
+        result = random_num_1 - random_num_2
+    elif random_symbol == '*':
+        result = random_num_1 * random_num_2
+
+    question = f'{random_num_1} {random_symbol} {random_num_2}'
+
+    return question, str(result)
+
+
+
+
+
+
+
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from brain_games.game_engine  import play
+from brain_games.games import calc
+
+
+def main():
+    play(calc)
+
+
+if __name__ == '__main__':
+    main()

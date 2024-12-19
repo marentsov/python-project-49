9d3f4578fb5efd920af80c4416b20362ebc5625e
marentsov <mr.marentsov@gmail.com>
2024-12-19 12:11:34 +0300

    add new game prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,26 @@
+from random import randint
+import math
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+def num_prime(num):
+    if num <= 1:
+        return False
+    num_sqrt = int(math.sqrt(num))
+    num_divisors = range(2, (num_sqrt + 1))
+    for item in num_divisors:
+        if num % item == 0:
+            return False
+    return True
+
+def generate_game():
+    question = randint(0, 100)
+    num = question
+    if num_prime(num):
+        result = 'yes'
+    else:
+        result = 'no'
+
+    return question, result
+
+
+
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.game_engine  import play
+from brain_games.games import prime
+
+
+def main():
+    play(prime)
+
+
+if __name__ == '__main__':
+    main()

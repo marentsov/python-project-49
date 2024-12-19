d63694c4474608ce90c5d0d81669dd540953d1a7
marentsov <mr.marentsov@gmail.com>
2024-12-19 12:27:11 +0300

    fixed game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -7,16 +7,19 @@ def play(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.RULES)
+
     for _ in range(ROUNDS):
         question, result = game.generate_game()
         print(f"Question: {question}")
         answer = prompt.string('Your answer: ')
+
         if result == answer:
             print('Correct!')
         else:
             print(f'\"{answer}" is wrong answer ;(. Correct answer was "{result}".')
             print(f"Let's try again, {name}!")
             break
+
     else:
         print(f'Congratulations, {name}!')
 

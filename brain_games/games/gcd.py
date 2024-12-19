ed4b51114daac25e40866d09e70751e7d629c961
marentsov <mr.marentsov@gmail.com>
2024-12-19 09:19:27 +0300

    add new game gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,15 @@
+import math
+from random import randint
+RULES = 'Find the greatest common divisor of given numbers.'
+
+
+def generate_game():
+
+    random_num_1 = randint(0, 100)
+    random_num_2 = randint(0, 100)
+
+    question = f'{random_num_1} {random_num_2}'
+
+    result = math.gcd(random_num_1, random_num_2)
+
+    return question, str(result)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.game_engine  import play
+from brain_games.games import gcd
+
+
+def main():
+    play(gcd)
+
+
+if __name__ == '__main__':
+    main()
